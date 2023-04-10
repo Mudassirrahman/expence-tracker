043e611a2eef17e8f9e215db95c88b0a6533a363
mudassirrahman <mudassirrehman1989@gmail.com>
2023-04-10 14:46:41 +0500

    css clean

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Expance1 from"./Expance1";
+import Expance from"./Expance";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Expance1 />
+    <App />
   </React.StrictMode>
 );
 
